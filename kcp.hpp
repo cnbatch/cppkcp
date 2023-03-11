0e6731624a4a5071eafc664d791b622bae5cc940
cnbatch <cnbatch@gmail.com>
2023-03-12 00:56:19 +0800

    flexible & readable

--- kcp.hpp
+++ kcp.hpp
@@ -111,12 +111,15 @@ namespace KCP
 			uint32_t rto = 0;
 			uint32_t fastack = 0;
 			uint32_t xmit = 0;
-			std::vector<char> data;
+			size_t len = 0;
+			std::unique_ptr<char[]> data = nullptr;
 
 			Segment() = default;
 			Segment(size_t sizes)
 			{
-				data.resize(sizes);
+				if (sizes > 0)
+					data = std::make_unique<char[]>(sizes);
+				len = sizes;
 			}
 
 			Segment(Segment &&other) noexcept
@@ -144,6 +147,7 @@ namespace KCP
 				this->rto = other.rto;
 				this->fastack = other.fastack;
 				this->xmit = other.xmit;
+				this->len = other.len;
 				this->data = std::move(other.data);
 			}
 		};
